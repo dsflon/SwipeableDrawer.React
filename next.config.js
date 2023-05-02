749673eeb3ca98dbc5b0ddeec8aa87b4f5ebe093
Daiki Saito <dsflon@gmail.com>
2023-05-02 19:22:27 +0900

    :feat: gh-pages にデプロイ

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
+const branchName = process.env.BRANCH_NAME ? "/" + process.env.BRANCH_NAME : "";
+
 const nextConfig = {
   reactStrictMode: true,
 
-  output: 'standalone',
+  assetPrefix: branchName,
 
-  // out/test/index.html のように出力するために必要
-  trailingSlash: true,
+  basePath: branchName,
+};
 
-  // next/image が next export に対応していないので、下記で対応
-  images: {
-    unoptimized: true,
-  },
-}
-
-module.exports = nextConfig
+module.exports = nextConfig;
